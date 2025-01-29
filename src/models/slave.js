9fc9883b4d7a291a036aab682e15e8a12eac5ace
KamOri1 <iknowthis45@gmail.com>
2025-01-29 11:44:15 +0100

    chore: czyszczenie folderu

--- src/models/slave.js
+++
@@ -1,9 +0,0 @@
-const slave = {
-  id: "162672",
-  master_sa: "150033",
-  offer_id: "219760",
-  username: "Beliani IT",
-  url: "beliani.it",
-  auction_name: "Beliani.it Konsolentisch CARCASSONNE Spiegeleffekt CH",
-  multi: "-1",
-};
