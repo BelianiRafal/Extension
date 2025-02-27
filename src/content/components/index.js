de4969105272bd1da1ac1b2a8a07ad0116d077de
Rafal <mailto:rafal.kobus@beliani.net>
2025-02-27 08:14:52 +0100

    Feat: Update: NL tel nr | Fix: Fullfill RO at CGB

--- src/content/components/index.js
+++ src/content/components/index.js
@@ -710,7 +710,7 @@ const COMPONENTS = {
       "nlnl": {
         "Company Details": "Bedrijfsinformatie",
         "Address": "Beliani (EU) GmbH 6340 Baar Switzerland",
-        "Mobile number": "Telefoonnummer: (038) 7440 103;",
+        "Mobile number": "Telefoonnummer: (020) 7871 308;",
         "Email address": "E-mail:",
         "Mail to": "mailto:mail@beliani.nl",
         "Email": "mail@beliani.nl",
