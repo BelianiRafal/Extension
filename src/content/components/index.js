c1c3fd079fe906275f9722e5a28c5a8c5c4603c8
Rafal <mailto:rafal.kobus@beliani.net>
2025-03-31 15:00:25 +0200

    Feat: New tel nr for FI

--- src/content/components/index.js
+++ src/content/components/index.js
@@ -860,7 +860,7 @@ const COMPONENTS = {
       "fifi": {
         "Company Details": "Yhtiön tiedot",
         "Address": "Beliani (DE) GmbH 6340 Baar Sveitsi",
-        "Mobile number": "Puhelinnumero: 09 4245 0464;",
+        "Mobile number": "Puhelinnumero: 09 3157 9611;",
         "Email address": "sähköposti:",
         "Mail to": "mailto:mail@beliani.fi",
         "Email": "mail@beliani.fi",
