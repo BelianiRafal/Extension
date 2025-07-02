dcc573fde1cf2db45d4ac68bd815ebe0416553bb
KamOri1 <iknowthis45@gmail.com>
2025-07-02 09:51:55 +0200

    feat(ProductListFilter): class to filter the list of products before downloading the json file

--- src/content/selectProductsNew.js
+++ src/content/selectProductsNew.js
@@ -1,6 +1,52 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   // sendData to service worker
   // sendResponse("data")
+class ProductListFilter {
+  constructor(products) {
+    this.products = products; 
+  }
+  processShopSAAlias(item) {
+    const processedAlias = {};
+    if (item && item.ShopSAAlias) {
+      for (const langKey in item.ShopSAAlias) {
+        if (item.ShopSAAlias.hasOwnProperty(langKey)) {
+          const aliasData = item.ShopSAAlias[langKey];
+          processedAlias[langKey] = {
+            language: aliasData.language,
+            id: aliasData.id,
+            value: aliasData.value,
+          };
+        }
+      }
+    }
+    return processedAlias; 
+  }
+  processAllProducts() {
+    return this.products.map((item) => {
+      const savedParams = item.saved_params || {};
+      const data = item.data || {};
+      const processedProduct = {
+        saved_params: {
+          master_sa: savedParams.master_sa,
+          username: savedParams.username,
+          ShopPrice: savedParams.ShopPrice,
+          ShopHPrice: savedParams.ShopHPrice,
+        },
+        data: {
+          id: data.id,
+          username: data.username,
+          master_sa: data.master_sa,
+        },
+        id: item.id,
+        article_name: item.article_name,
+      };
+      if (data.username === "Beliani") {
+        processedProduct.ShopSAAlias = this.processShopSAAlias(item);
+      }
+      return processedProduct;
+    });
+  }
+}
 
   (async () => {
     if (message.action === "download_products") {
@@ -16,23 +62,27 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       }
       try {
         const { inActiveProducts, isInactiveProducts } = isActive(products);
+
+        const active_filter_products = new ProductListFilter(products).processAllProducts();
+        const inActive_filter_products = new ProductListFilter(inActiveProducts).processAllProducts();
+
         if (isInactiveProducts) {
           if (confirm("Inactive products found. Would you like to continue?")) {
             downloadJSON({ data: products, name: "products.json" });
             downloadJSON({
-              data: inActiveProducts,
+              data: inActive_filter_products,
               name: "inactive_products.json",
             });
             return;
           } else {
             downloadJSON({
-              data: inActiveProducts,
+              data: inActive_filter_products,
               name: "inactive_products.json",
             });
             return;
           }
         }
-        downloadJSON({ data: products, name: "products.json" });
+        downloadJSON({ data: active_filter_products, name: "products.json" });
         sendResponse({ status: "success" });
       } catch (error) {
         alert(
