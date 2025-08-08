6ff9caed09fb3b34378b49fcdc8a86821c2f9fa2
kkazzann <contact@kznlabs.com>
2025-08-08 14:47:02 +0200

    feat(login): added automatic timestamp login approval

---
+++ src/content/autoTimeStamp.js
@@ -0,0 +1,63 @@
+const toggleAutoAcceptButton = document.createElement("button");
+toggleAutoAcceptButton.innerText = "Toggle Auto Confirm";
+
+function updateButtonStyle() {
+  const isActive = localStorage.getItem("autoAcceptLogin") === "true";
+  toggleAutoAcceptButton.style = `
+    color: ${isActive ? "green" : "red"};
+    font-family: Arial;
+    text-transform: uppercase;
+    font-size: 11px;
+    background: white;
+    border: 2px solid ${isActive ? "green" : "red"};
+    padding: 5px 10px;
+    cursor: pointer;
+    border-radius: 3px;
+    margin: 5px;`;
+}
+
+updateButtonStyle();
+
+toggleAutoAcceptButton.onclick = () => {
+  const currentState = localStorage.getItem("autoAcceptLogin") === "true";
+  localStorage.setItem("autoAcceptLogin", currentState ? "false" : "true");
+  updateButtonStyle(); // Update button appearance after click
+};
+
+document.querySelector("#timesheet_div").after(toggleAutoAcceptButton);
+
+function handleTimestampConfirm() {
+  const alertifyHeaders = document.querySelectorAll(".ajs-header");
+
+  if (alertifyHeaders.length < 2) return;
+
+  // skip weird empty alertify element
+  const timestampAlert = alertifyHeaders[1];
+
+  // check if this is the correct alert
+  if (!timestampAlert) return;
+  if (timestampAlert.innerText.trim() !== "Login Confirmation") return;
+
+  // extract the "YES" button
+  const yesButton =
+    timestampAlert?.nextElementSibling?.nextElementSibling?.children?.[1]
+      ?.children?.[1];
+
+  yesButton.click();
+
+  // remove observer since we are logged in
+  if (window.__loginConfObserver) {
+    window.__loginConfObserver.disconnect();
+  }
+}
+
+window.__loginConfObserver = new MutationObserver(() => {
+  if (localStorage.getItem("autoAcceptLogin") === 'true') handleTimestampConfirm();
+});
+
+window.__loginConfObserver.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+
+if (localStorage.getItem("autoAcceptLogin") === 'true') handleTimestampConfirm();
